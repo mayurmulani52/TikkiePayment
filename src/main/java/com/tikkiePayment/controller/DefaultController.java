cd789c319e378097b3e69d4646fb1cc7f0eda341
Mayur Mulani <mayur@doctoranywhere.com>
2020-10-04 01:07:39 +0800

    welcome URL tested with Okta Auth profile tag

--- src/main/java/com/tikkiePayment/controller/DefaultController.java
+++ src/main/java/com/tikkiePayment/controller/DefaultController.java
@@ -1,16 +1,20 @@
 package com.tikkiepayment.controller;
 
+import java.security.Principal;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/tikkie-payment")
 public class DefaultController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Hello, Welcome to Tikkie Payment!";
+	@PreAuthorize("#oauth2.hasScope('profile')")
+    @GetMapping("/welcome")
+    public String home(Principal principal) {
+        return "Hello, Welcome to Tikkie Payment!"+ principal.getName();
     }
 
 }
