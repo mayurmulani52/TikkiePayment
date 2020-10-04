f81314e658839e09598039079c5fb42f0aa50a1c
Mayur Mulani <mayur@doctoranywhere.com>
2020-10-04 19:43:43 +0800

    tikkie payment postman json added and tikkiet payment rest controller

---
+++ src/main/java/com/tikkiePayment/controller/TikkiePaymentRestController.java
@@ -0,0 +1,10 @@
+package com.tikkiepayment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/tikkie-payment")
+public class TikkiePaymentRestController {
+
+}
