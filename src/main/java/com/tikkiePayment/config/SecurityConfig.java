3c01eed9ad9116898ad47fe3597f59aa6dfe1e78
Mayur Mulani <mayur@doctoranywhere.com>
2020-10-04 19:39:00 +0800

    okta configuration updated and added tiikkie-payment rest controller

--- src/main/java/com/tikkiePayment/config/SecurityConfig.java
+++ src/main/java/com/tikkiePayment/config/SecurityConfig.java
@@ -4,8 +4,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-
 @EnableWebSecurity
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig{}
+public class SecurityConfig  {}
--- src/main/java/com/tikkiePayment/controller/DefaultController.java
+++ src/main/java/com/tikkiePayment/controller/DefaultController.java
@@ -1,18 +1,23 @@
 package com.tikkiepayment.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import java.security.Principal;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/tikkie-payment")
 public class DefaultController {
 
-    @GetMapping("/welcome")
-    public String home(@AuthenticationPrincipal OidcUser user) {
-        return "Hello, Welcome "+ user.getFullName() +" to Tikkie Payment!";
+	@GetMapping("/")
+    public String welcome(){
+        return "Hello, Welcome to Tikkie Payment Application!";
     }
-
+	
+	@PreAuthorize("#oauth2.hasScope('profile')")
+    @GetMapping("/protected/")
+    public String helloWorldProtected(Principal principal) {
+        return "Hello VIP " + principal.getName();
+    }
+	
 }
