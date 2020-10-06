48036b1a6282a29a9282141263a0369596811cbb
Mayur Mulani <mayur@doctoranywhere.com>
2020-10-07 00:50:46 +0800

    transformer layer added

---
+++ src/main/java/com/tikkiePayment/transformer/TikkiePaymentTransformer.java
@@ -0,0 +1,10 @@
+package com.tikkiepayment.transformer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TikkiePaymentTransformer {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TikkiePaymentTransformer.class);
+
+}
